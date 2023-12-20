af52fef8c0a9e81fff264b5b412665f0b3f95cff
abenedetti27 <145300062+abenedetti27@users.noreply.github.com>
2023-12-19 22:11:33 -0800

    Add employee and role tables to database schema

--- db/scheme.sql
+++ db/scheme.sql
@@ -7,3 +7,21 @@ CREATE TABLE department (
   department_id INT AUTO_INCREMENT PRIMARY KEY,
   department_name VARCHAR(50) NOT NULL
 );
+
+CREATE TABLE employee (
+  employee_id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(role_id),
+  FOREIGN KEY (manager_id) REFERENCES employee(employee_id)
+);
+
+CREATE TABLE role (
+  role_id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(50) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(department_id)
+);
