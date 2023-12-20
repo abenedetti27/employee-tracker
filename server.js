f3648263e90aa72faeaab2e5c862a5e728a79801
abenedetti27 <145300062+abenedetti27@users.noreply.github.com>
2023-12-19 22:00:57 -0800

    Add startApp function to server.js

--- server.js
+++ server.js
@@ -7,3 +7,24 @@ const connection = mysql.createConnection({
     password: '',
     database: 'employee_db'
 });
+
+function startApp() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'start',
+                message: 'What would you like to do?',
+                choices: [
+                    'Add Department',
+                    'Add Role',
+                    'Add Employee',
+                    'View Departments',
+                    'View Roles',
+                    'View Employees',
+                    'Update Employee Role',
+                    'Exit'
+                ]
+            }
+        ])
+}
